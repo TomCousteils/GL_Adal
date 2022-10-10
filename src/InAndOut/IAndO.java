740b9a26a5c376e0c759874e4893342804ab0fc4
Blackbun138 <maligued13@gmail.com>
2022-10-10 17:19:47 +0200

    Début d'implémentation de handleKeyPressed

--- src/InAndOut/IAndO.java
+++ src/InAndOut/IAndO.java
@@ -1,5 +1,16 @@
 package InAndOut;
 
+import java.awt.event.KeyEvent;
+
 public class IAndO {
+
+    private Object kernel =  new Object();
+
+    public void handleKeyPressed(KeyEvent keyEvent){
+        switch (keyEvent.getKeyChar()){
+            case 'K' -> {
+                return;}
+        }
+    }
 
 }
