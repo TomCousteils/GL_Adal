1f1f375c5247fd08446b9de3dae4f64461627756
Blackbun138 <maligued13@gmail.com>
2022-10-12 00:47:00 +0200

    Implémentation de keyPressed, et KeyReleased de l'interface keyListener. La classe Ian0 doit étendre Jframe. Il est possible que keyReleased ne serve, mais au moins on sait comment il fonctionne. De plus je n'ai pas compris ce que faisait keyTyped mais je ne suis pas sur que cela nous soit utile. Possibilité d'implémenter une entrée à la souris ??

--- src/InAndOut/IAndO.java
+++ src/InAndOut/IAndO.java
@@ -1,29 +1,49 @@
 package InAndOut;
 
+import javax.swing.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class IAndO {
+
+public class IAndO extends JFrame implements KeyListener {
 
     private Object kernel =  new Object();
 
-    public void handleKeyPressed(KeyEvent keyEvent){
-        switch (keyEvent.getKeyChar()){
-            case 'K' -> {
-            }
-            case 'M' -> {
-                System.out.println("Niques ta mère");
-            }
+    public IAndO (){
+        addKeyListener(this);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_Q -> System.out.println("Lances un Q-Spell");
+            case KeyEvent.VK_W -> System.out.println("Lances son W");
+            case KeyEvent.VK_E -> System.out.println("Lances son E");
+            case KeyEvent.VK_R -> System.out.println("Ultimeeeee");
+        }
+
+
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_Q -> System.out.println("Lances un Q-Spell");
+            case KeyEvent.VK_LEFT -> System.out.println("Vas a gauche");
+            case KeyEvent.VK_RIGHT -> System.out.println("Vas a droite");
+            case KeyEvent.VK_UP -> System.out.println("Vas en haut");
+            case KeyEvent.VK_DOWN -> System.out.println("Vas en bas");
         }
     }
 
-    public void handleKeyReleased(KeyEvent keyEvent){
-        switch (keyEvent.getKeyChar()){
-            case 'K' -> {
-            }
-            case 'M' -> {
-                System.out.println("En fait elle est gentille ta grosse daronne");
-            }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT -> System.out.println("S'arrete d'aller a gauche");
+            case KeyEvent.VK_RIGHT -> System.out.println("S'arrete d'aller a droite");
+            case KeyEvent.VK_UP -> System.out.println("S'arrete d'aller en haut");
+            case KeyEvent.VK_DOWN -> System.out.println("S'arrete d'aller en bas");
+
         }
     }
-
 }
--- src/InAndOut/Main.java
+++ src/InAndOut/Main.java
@@ -1,8 +1,15 @@
 package InAndOut;
 
+import java.awt.event.KeyEvent;
+import java.beans.EventHandler;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("I am a Geek");
+        IAndO iAndO = new IAndO();
+        while (true){
+            iAndO.setVisible(true);
+        }
     }
 }
