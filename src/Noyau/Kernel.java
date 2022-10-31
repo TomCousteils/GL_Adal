75bd4ba00e95775c1ccab398e93ef57d17ac9102
Nephitys <oticapsed.despacito@gmail.com>
2022-10-31 14:40:40 +0100

    oui3

--- src/Noyau/Kernel.java
+++ src/Noyau/Kernel.java
@@ -1,5 +1,8 @@
 package Noyau;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Kernel implements Graphique,Physique,In_Out{
 
     private String log="";
@@ -7,6 +10,8 @@ public class Kernel implements Graphique,Physique,In_Out{
     public Object Sprite; // je sais pas trop comment introduire ça
 
     public Object Coord; //créer une classe coordonnée ?
+
+    public ArrayList<Object> allCoord;
 
     private Object Pressed;
 
@@ -20,9 +25,10 @@ public class Kernel implements Graphique,Physique,In_Out{
         log=log+"Kernel asked for pressed keys \n";
         return Pressed;
     }
-    public Object getAllPosition(){
+    public ArrayList<Object> getAllPosition(){
+        //allCoord=nom de la fonction de physique qui envoie les coordonnés
         log=log+"Kernel asked for all positions \n";
-        return Coord;
+        return allCoord;
     }
 
     public Object giveAllSprite(){
@@ -40,6 +46,23 @@ public class Kernel implements Graphique,Physique,In_Out{
         //Objet=nom de la fonction de gameplay
         log=log+"Kernel asked for the new Object \n";
         return Objet;
+    }
+
+    public Object sendASprite(int id_obj) {
+        File dir = new File("C:\\Users\\PC\\Desktop\\Dossier");
+        File[] liste = dir.listFiles();
+        for (File item : liste) {
+            if (item.isFile() && Integer.parseInt(item.getName()) == id_obj) {
+                log = log + "Graphic asked for the new Object \n";
+                return ("C:\\Users\\PC\\Desktop\\Dossier" + id_obj);
+
+            }
+        }
+        return("Pas de sprite associé");
+    }
+
+    public Object sendASpriteBIS(int id_obj) {
+        return ("C:\\Users\\PC\\Desktop\\Dossier" + id_obj);
     }
 
     public String getLog() {
